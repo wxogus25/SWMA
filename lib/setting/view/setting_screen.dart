f4dc8b7c0bd60830ba2d0191c534a28159125717
Taehyeon Jeong <wxogus25@gmail.com>
2022-10-02 01:34:32 +0900

    setting page

--- lib/setting/view/setting_screen.dart
+++ lib/setting/view/setting_screen.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
   @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  final app = AppState(true, '');
+
+  @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Setting"),);
+    if (app.loading) return _loading();
+    if (app.user.isEmpty) return _signIn();
+    return _main();
   }
+
+  _delay() {
+    Future.delayed(Duration(seconds: 1), () {
+      setState(() => app.loading = false);
+    });
+  }
+
+  Widget _loading() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _signIn() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text('id'),
+          Text('pass'),
+          ElevatedButton(
+            child: Text('login'),
+            onPressed: () {
+              setState(() {
+                app.loading = true;
+                app.user = 'my name';
+                _delay();
+              });
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _main() {
+    return Center(
+      child: IconButton(
+        icon: Icon(Icons.account_circle),
+        onPressed: () {
+          setState(() {
+            app.user = '';
+            app.loading = true;
+            _delay();
+          });
+        },
+      ),
+    );
+  }
+}
+
+class AppState {
+  bool loading;
+  String user;
+  AppState(this.loading, this.user);
 }

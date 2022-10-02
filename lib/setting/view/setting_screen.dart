58e8394fa86c169f0a46fd8838f9228def1e81aa
Taehyeon Jeong <wxogus25@gmail.com>
2022-10-03 02:30:00 +0900

    google login in android

--- lib/setting/view/setting_screen.dart
+++ lib/setting/view/setting_screen.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);

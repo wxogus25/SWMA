9332eb4445977bab58c11617c7b9ba48cc2851c3
Taehyeon Jeong <wxogus25@gmail.com>
2022-11-10 16:58:24 +0900

    notifyView tile list

--- tot/lib/common/view/notify_view.dart
+++ tot/lib/common/view/notify_view.dart
@@ -1,18 +1,106 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:graphview/GraphView.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:tot/common/component/news_tile.dart';
+import 'package:tot/common/const/colors.dart';
+import 'package:tot/common/const/padding.dart';
+import 'package:tot/common/data/API.dart';
+import 'package:tot/common/data/cache.dart';
 import '../layout/default_layout.dart';
 
-class NotifyView extends StatelessWidget {
+class NotifyView extends StatefulWidget {
   const NotifyView({Key? key}) : super(key: key);
+
+  @override
+  State<NotifyView> createState() => _NotifyViewState();
+}
+
+class _NotifyViewState extends State<NotifyView> {
+  RefreshController _controller = RefreshController();
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-        isExtraPage: true,
-        pageName: "알림",
-        child: Container());
+      pageName: "알림",
+      isExtraPage: true,
+      child: FutureBuilder(
+        future: tokenCheck(() => API.getNewsListByFilter(userFilterKey)),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData == false)
+            return Center(child: CircularProgressIndicator());
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: HORIZONTAL_PADDING.w),
+            // padding: const EdgeInsets.fromLTRB(HORIZONTAL_PADDING, 0, 5 ,0),
+            child: StatefulBuilder(
+              builder: (BuildContext context2, setter) {
+                return _refresher(snapshot.data, setter);
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _refresher(data, setter) {
+    return SlidableAutoCloseBehavior(
+      child: SmartRefresher(
+        controller: _controller,
+        onRefresh: () async {
+          var _next = null;
+          _next = await tokenCheck(() => API.getNewsListByFilter(userFilterKey));
+          _controller.refreshCompleted();
+          data = _next;
+          setter(() {});
+        },
+        onLoading: () async {
+          var _next = null;
+          if (!data.isEmpty) {
+            await tokenCheck(() => API.getNewsListByFilter(userFilterKey, newsId: data.last.id));
+          }
+          _controller.loadComplete();
+          if (_next != null) {
+            data.addAll(_next!);
+          }
+          setter(() {});
+        },
+        enablePullUp: true,
+        enablePullDown: true,
+        child: ListView.separated(
+          itemBuilder: (context, i) {
+            if (i == 0) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Text(
+                    "아직 못 본 관심 뉴스",
+                    style: TextStyle(
+                        fontSize: 28.sp,
+                        color: PRIMARY_COLOR,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 15.h,
+                  ),
+                ],
+              );
+            }
+            return NewsTile.fromData(data[i - 1]);
+          },
+          separatorBuilder: (context, i) {
+            if (i == 0) return SizedBox.shrink();
+            return const Divider(
+              thickness: 1.5,
+            );
+          },
+          itemCount: data.length + 1,
+          physics: ClampingScrollPhysics(),
+        ),
+      ),
+    );
   }
 }

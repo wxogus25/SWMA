f290530151d5d34e9e3e58470d1ff8ddef138de5
Taehyeon Jeong <wxogus25@gmail.com>
2022-11-12 15:11:36 +0900

    format change

--- tot/lib/common/data/AppController.dart
+++ tot/lib/common/data/AppController.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
